a94b68aacc0e335f5423e9699228f1913303496e
Nafissan <101016522+Nafissan@users.noreply.github.com>
2022-07-27 14:15:17 +0000

    Update Hackage.java

--- Hackage.java
+++ Hackage.java
@@ -6,19 +6,11 @@ public class Hackage{
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Renseignez votre identifiant!");
-		String login=sc.nextLine();
-		System.out.println("Renseignez votre mot de passe!");
-		String password=sc.nextLine();
-		int taille=password.length();
-		Compte com=new Compte();
-		com.setLogin(login);
-		com.setPass(password);
 		System.out.println("Choisissez une methode (1 ou 2)!");
 		int way=sc.nextInt();
 		long timer=System.currentTimeMillis();
 		Hacking cracker=FactoryHacking.hackingWay(way);
-		cracker.CrackerWay(com);
+		cracker.CrackerWay();
 		System.out.println(((System.currentTimeMillis()-timer))+"ms");
 	}
 }
